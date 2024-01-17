71853d7afb9735594d290701355a20db233d4a3a
seablood <dudwnszero99@gmail.com>
2024-01-17 22:33:33 +0900

    test2

--- simproject/src/main/java/org/project/simproject/repository/CommentRepository.java
+++ simproject/src/main/java/org/project/simproject/repository/CommentRepository.java
@@ -4,9 +4,11 @@ import org.project.simproject.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findByArticleId(Long articleId);
+    List<Comment> findByNicknameContains(String nickname);
 }
