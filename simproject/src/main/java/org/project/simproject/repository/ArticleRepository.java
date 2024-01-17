089b6f9a343ccf6007f053311de4249c39a27b05
seablood <dudwnszero99@gmail.com>
2024-01-17 22:06:09 +0900

    10

--- simproject/src/main/java/org/project/simproject/repository/ArticleRepository.java
+++ simproject/src/main/java/org/project/simproject/repository/ArticleRepository.java
@@ -4,7 +4,9 @@ import org.project.simproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+    List<Article> findByAuthorContains(String author);
 }
