a5b15e380c83d49aba48eb03ac72b19f2387629b
seablood <dudwnszero99@gmail.com>
2024-01-17 19:23:10 +0900

    9

--- simproject/src/main/java/org/project/simproject/domain/Bookmark.java
+++ simproject/src/main/java/org/project/simproject/domain/Bookmark.java
@@ -13,7 +13,7 @@ public class Bookmark {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
-    @Column(name = "articleId", nullable = false)
+    @Column(name = "articleId")
     private Long articleId;
     @Column(name = "userId", nullable = false)
     private Long userId;
